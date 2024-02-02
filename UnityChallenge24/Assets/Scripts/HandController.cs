c7a3f1d40df1cf1b5ffa7520784fa16c45dc4d56
L42ARO <89555610+L42ARO@users.noreply.github.com>
2024-02-01 21:05:47 -0500

    Mod script

--- UnityChallenge24/Assets/Scripts/HandController.cs
+++ UnityChallenge24/Assets/Scripts/HandController.cs
@@ -4,23 +4,12 @@ using UnityEngine;
 
 public class HandController : MonoBehaviour
 {
-    public float speed = 1.0f; // Speed of movement
-    private float timer = 0.0f;
-    private bool movingPositive = true;
-
+    void Start()
+    {
+        
+    }
     void Update()
     {
-        timer += Time.deltaTime; // Increase timer with time passed
-
-        // If timer reaches 10 seconds, change direction and reset timer
-        if (timer >= 2f)
-        {
-            movingPositive = !movingPositive;
-            timer = 0.0f;
-        }
-
-        // Move the object along the z-axis based on direction
-        float direction = movingPositive ? 1.0f : -1.0f;
-        transform.Translate(0.0f, 0.0f, direction * speed * Time.deltaTime);
+        
     }
 }
