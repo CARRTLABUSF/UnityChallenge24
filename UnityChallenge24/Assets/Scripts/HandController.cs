2358696b62a155a95a713858f6e87d966a2b9615
jegert0 <jegertm@gmail.com>
2024-02-03 18:23:19 -0500

    completed lvl 1

--- UnityChallenge24/Assets/Scripts/HandController.cs
+++ UnityChallenge24/Assets/Scripts/HandController.cs
@@ -1,15 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class HandController : MonoBehaviour
-{
-    void Start()
-    {
-        
-    }
+{   
+    //variable will store how fast hand will move
+    [SerializeField] int speed;
+   
     void Update()
-    {
+    {   
+        //keeps current x and y and uses sine wave to ossiclate z position
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Sin(Time.time - 1 * speed));
         
     }
+
+   
 }
--- UnityChallenge24/Assets/Scripts/LaserHand.cs
+++ UnityChallenge24/Assets/Scripts/LaserHand.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class LaserHand : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
+    float range = 3;
+    bool firstCheck = false;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        checkCollision();
     }
+
+    void checkCollision(){
+        
+
+        Ray ray = new Ray(transform.position, new Vector3(-1 * range, 0, 0));
+        Debug.DrawRay(ray.origin, ray.direction * range, Color.red);
+
+        if(Physics.Raycast(ray, out RaycastHit hit, range)){
+
+            if (hit.collider.tag == "Destroyable" && !firstCheck){
+
+                firstCheck = true;
+                print("collision detected");
+                hit.collider.gameObject.SetActive(false);
+        
+            }
+            
+        } 
+        else firstCheck = false;
+    }
+
 }
