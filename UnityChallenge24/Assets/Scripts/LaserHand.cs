8b9f6ebc322f1dcf998896baa0f3dc6d9e7e42bf
Rakshith Poojary <rakshithp7@gmail.com>
2024-02-03 19:23:23 -0500

    Challenge completed
    Completed all 3 levels along with the extras.

--- UnityChallenge24/Assets/Scripts/LaserHand.cs
+++ UnityChallenge24/Assets/Scripts/LaserHand.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class LaserHand : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float rayLength = 5f; // Length of the ray
+    private bool isHittingGameObject = false; // Flag to track if currently hitting the sphere
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Cast a ray forward from the hand's position
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(new Vector3(-1, 0, 0)) * rayLength;
+        Debug.DrawRay(transform.position, forward, Color.red); // Visualize the ray in the editor
+
+        if (Physics.Raycast(transform.position, forward, out hit, rayLength))
+        {
+            if (hit.collider.gameObject.CompareTag("Destroyable"))
+            {
+                if (!isHittingGameObject) // If it wasn't already hitting the sphere
+                {
+                    // Remove one life from the GameObject that was hit
+                    hit.collider.gameObject.GetComponent<SphereController>().removeLife();
+                    if (hit.collider.gameObject.GetComponent<SphereController>().getLives() <= 0)
+                    {
+                        Destroy(hit.collider.gameObject);
+                        Debug.Log("Destroyed " + hit.collider.gameObject.name);
+                    }
+                    else
+                    {
+                        Debug.Log("Damage Taken! Life: " + hit.collider.gameObject.GetComponent<SphereController>().getLives());
+                    }
+                    isHittingGameObject = true; // Set flag to true
+                }
+            }
+            else
+            {
+                // If the ray is not hitting the sphere object anymore
+                if (isHittingGameObject)
+                {
+                    isHittingGameObject = false;
+                }
+            }
+        }
     }
 }
