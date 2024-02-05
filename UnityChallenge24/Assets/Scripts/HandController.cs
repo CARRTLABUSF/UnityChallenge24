07edc2a8e3645e11823a931a395d77d97d236302
SHUB2205 <111409237+SHUB2205@users.noreply.github.com>
2024-02-05 15:10:15 -0500

    Update HandController.cs (Level 1)

--- UnityChallenge24/Assets/Scripts/HandController.cs
+++ UnityChallenge24/Assets/Scripts/HandController.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 
 public class HandController : MonoBehaviour
 {
+    public float moveSpeed = 5.0f; 
+    public float moveDistance = 1.0f; 
+
+    private Vector3 initialPosition;
+    private bool moving = true;
+
     void Start()
     {
-        
+        initialPosition = transform.position;
     }
+
     void Update()
     {
         
+        MoveHand();
+    }
+
+    void MoveHand()
+    {
+        if (moving)
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime*-1);
+        }
+
+        
+        if (Mathf.Abs(transform.position.z - initialPosition.z) >= moveDistance)
+        {
+            
+            moving = !moving;
+        }
     }
 }
