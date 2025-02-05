8ce9a3473a73186b717dcc5d977acd101344730a
Danyl Bulbas <64973521+BulbasDanil@users.noreply.github.com>
2025-02-04 23:31:53 -0500

    Completed Task 3. Made GameManager a Singleton, added Health to Sphere

--- UnityChallenge24/Assets/Scripts/GameManager.cs
+++ UnityChallenge24/Assets/Scripts/GameManager.cs
@@ -1,17 +1,44 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+    
     public GameObject skipperPosition;
     public GameObject skateboardPosition;
 
     [SerializeField] private GameObject SkipperPrefab;
     [SerializeField] private GameObject SkateboardPrefab;
     
+    // Initializing Singleton 
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+    
     void Start()
     {
         Instantiate(SkipperPrefab, skipperPosition.transform);
         Instantiate(SkateboardPrefab, skateboardPosition.transform);
+    }
+
+    public void Restart()
+    {
+        StartCoroutine(RestartScene());
+    }
+    
+    private IEnumerator RestartScene()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
     }
     
 }
--- UnityChallenge24/Assets/Scripts/SphereController.cs
+++ UnityChallenge24/Assets/Scripts/SphereController.cs
@@ -1,9 +1,36 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class SphereController : MonoBehaviour
 {
+    private int _health = 5;
+    private bool _canTakeDamage = true;
+
     public void TakeDamage()
     {
-        Destroy(gameObject);
+        if (_canTakeDamage)
+        {
+            _health -= 1;
+
+            if (_health <= 0)
+            {
+                GameManager.Instance.Restart();
+                Destroy(gameObject);
+            }
+            else
+            {
+                StartCoroutine(DamageCooldown()); 
+            }
+        }
     }
+
+    private IEnumerator DamageCooldown()
+    {
+        _canTakeDamage = false;
+        yield return new WaitForSeconds(1f);
+        _canTakeDamage = true;
+    }
+
+
 }
