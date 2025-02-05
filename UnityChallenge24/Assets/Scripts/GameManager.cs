f11512e2baeb8e862d80bc1448679e439234cc4c
Danyl Bulbas <64973521+BulbasDanil@users.noreply.github.com>
2025-02-04 23:00:21 -0500

    Completed Task 2. Added prefab instantiation in GameManager.cs

--- UnityChallenge24/Assets/Scripts/GameManager.cs
+++ UnityChallenge24/Assets/Scripts/GameManager.cs
@@ -1,20 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject skipperPosition;
     public GameObject skateboardPosition;
-    // Start is called before the first frame update
+
+    [SerializeField] private GameObject SkipperPrefab;
+    [SerializeField] private GameObject SkateboardPrefab;
+    
     void Start()
     {
-
+        Instantiate(SkipperPrefab, skipperPosition.transform);
+        Instantiate(SkateboardPrefab, skateboardPosition.transform);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
